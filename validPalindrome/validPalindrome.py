7cd1015bd264e55ee431a9db5723c1f83bb8673c
Francisco Farinha <fquico1999@gmail.com>
2022-06-18 13:26:40 -0700

    added different solution to isPalindrome

--- validPalindrome/validPalindrome.py
+++ validPalindrome/validPalindrome.py
@@ -9,17 +9,31 @@ Given a string s, return true if it is a palindrome, or false otherwise.
 """ 
 def isPalindrome(s: str) -> bool:
         
-	# Filter out non alph characters based on ord
-	s = ''.join([char for char in s.lower() if ((ord(char) >= ord('a') and ord(char) <= ord("z"))) or (ord(char) >= ord('0') and ord(char) <= ord('9'))])
+    # Filter out non alph characters based on ord
+    s = ''.join([char for char in s.lower() if ((ord(char) >= ord('a') and ord(char) <= ord("z"))) or (ord(char) >= ord('0') and ord(char) <= ord('9'))])
 
-	if len(s) <= 1:
-		return True
+    if len(s) <= 1:
+        return True
 
-	# Compare two pointers vs halving a list of chars and reversing. 
-	for i in range(len(s)):
-		j = len(s)-i-1
+    # Compare two pointers vs halving a list of chars and reversing. 
+    for i in range(len(s)):
+        j = len(s)-i-1
 
-		if i >= j:
-	    	return True
-		if s[i] != s[j]:
-	    	return False
+        if i >= j:
+            return True
+        if s[i] != s[j]:
+            return False
+
+
+def isPalindromeList(s: str) -> bool:
+    
+    # Filter out non alph characters based on ord
+    s = ''.join([char for char in s.lower() if ((ord(char) >= ord('a') and ord(char) <= ord("z"))) or (ord(char) >= ord('0') and ord(char) <= ord('9'))])
+
+    return list(s) == list(s)[::-1]
+
+if __name__ == "__main__":
+
+    s = "A man, a plan, a canal: Panama"
+    print(isPalindrome(s))
+    print(isPalindromeList(s))
