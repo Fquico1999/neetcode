1c09abe5d02198ee4c93475183b83cc67648cde1
Francisco Farinha <fquico1999@gmail.com>
2022-07-02 00:13:33 -0700

    Added faster solution to checkInclusiojn

--- checkInclusion/checkInclusion.py
+++ checkInclusion/checkInclusion.py
@@ -28,11 +28,39 @@ def checkInclusion(s1: str, s2: str) -> bool:
 
 	return False
 
+def checkInclusionII(s1: str, s2:str)-> bool:
+	charCount = {}
+
+	# First, want to create a Hashset for the substring
+	subCharCount = {}
+	for char in s1:
+		subCharCount[char] = 1+subCharCount.get(char, 0)
+
+	l = 0
+
+	for r in range(len(s2)):
+		charCount[s2[r]] = 1 + charCount.get(s2[r], 0)
+
+		while (r-l+1) > len(s1):
+			charCount[s2[l]] -=1
+			if charCount[s2[l]] == 0:
+				charCount.pop(s2[l])
+			l+=1
+
+		if charCount.items() == subCharCount.items():
+			return True
+		else:
+			r+=1
+	return False
+
+
+
+
 if __name__  == "__main__":
 
 	s1 = "abc"
 	s2 = "eidbaoocbao"
-	print(checkInclusion(s1, s2))
+	print(checkInclusionII(s1, s2))
 
 
 
