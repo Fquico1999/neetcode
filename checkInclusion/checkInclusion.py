b2c519171b3a9b0499f36eff4d1346a012d882be
Francisco Farinha <fquico1999@gmail.com>
2022-07-02 01:38:11 -0700

    Added faster solution to checkInclusion by avoiding repeated HashMap comparison

--- checkInclusion/checkInclusion.py
+++ checkInclusion/checkInclusion.py
@@ -14,7 +14,7 @@ def checkInclusion(s1: str, s2: str) -> bool:
 
 	if len(s1) > len(s2):
 		return False
-	
+
 	sub = sorted(s1) # Should be O(nlogn)
 
 	l,r = 0, len(sub)-1
@@ -79,14 +79,62 @@ def checkInclusionII(s1: str, s2:str)-> bool:
 	# Overall time complexity is ~O(n), but we can improve from ~O(26n) to O(26+n) by using a variable to track matches.
 	return False
 
+def checkInclusionIII(s1: str, s2:str) -> bool:
+	"""
+	Here, the premise is to avoid checking the two HashMaps, which is at worst O(26), and instead keep track of matches.
+	"""
 
+	if len(s1) > len(s2):
+		return False
 
+	charCount = {}
+	subCharCount = {}
+	matches = 0
+	MAXMATCHES = 26 # This constant represents how many matches are needed for both HashMaps to match. Note that this only holds since the input strings are limited to a-z lowercase.
+
+	for i in range(len(s1)):
+		charCount[s2[i]] = 1 + charCount.get(s2[i], 0)
+		subCharCount[s1[i]] = 1 + subCharCount.get(s1[i],0)
+
+	for i in range(ord('a'), ord('z')+1):
+		charCount[chr(i)] = charCount.get(chr(i), 0)
+		subCharCount[chr(i)] = subCharCount.get(chr(i), 0)
+		if charCount[chr(i)] == subCharCount[chr(i)]:
+			matches+=1
+	l = 0
+	for r in range(len(s1), len(s2)):
+		# First, check if we have all the matches
+		if matches == MAXMATCHES:
+			return True
+
+		#Next, update charCount for the right pointer
+		charCount[s2[r]] += 1
+		# Update Matches
+		# If charCount is greater by one than subCount, that means they no longer match
+		if charCount[s2[r]] == subCharCount[s2[r]] +1:
+			matches -= 1
+		elif charCount[s2[r]] == subCharCount[s2[r]]:
+			matches +=1
+
+		# Next, update charCount for left pointer
+		charCount[s2[l]]-=1
+		# Update Matches
+		# If the charCount is less than subCount by one, they are no longer matches
+		if charCount[s2[l]] == subCharCount[s2[l]] -1:
+			matches-=1
+		elif charCount[s2[l]] == subCharCount[s2[l]]:
+			matches+=1
+
+		l+=1
+
+	#Since we only check at the start, check here as well.
+	return matches == MAXMATCHES
 
 if __name__  == "__main__":
 
-	s1 = "a"
-	s2 = "ab"
-	print(checkInclusionII(s1, s2))
+	s1 = "abc"
+	s2 = "eidbaoocoocbo"
+	print(checkInclusionIII(s1, s2))
 
 
 
