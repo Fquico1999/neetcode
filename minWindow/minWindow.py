f0fc683f25d143a958a98c943bf943b75b27d37e
Francisco Farinha <fquico1999@gmail.com>
2022-07-02 14:22:04 -0700

    Working solution to minWindow

--- minWindow/minWindow.py
+++ minWindow/minWindow.py
@@ -23,7 +23,7 @@ def minWindow(s: str, t: str) -> str:
         return True
 
     minWindow = ""
-    minSize = len(s)+1 #Need this in order to update minWindow when len(minWindow) == len(s)
+    minSize = len(s)+1
 
     if len(t) > len(s):
         return minWindow
@@ -32,35 +32,31 @@ def minWindow(s: str, t: str) -> str:
     subCharCount = {}
 
     # Char count for t
-    # Can improve slightly by also doing char count for s, up to len(t)
     for i in range(len(t)):
         subCharCount[t[i]] = 1 + subCharCount.get(t[i], 0)
-        charCount[s[i]] = 1 + charCount.get(s[i], 0)
 
 
     l = 0
-    #Start one before to ensure we check the status after setting the HashMaps
-    for r in range(len(t)-1, len(s)):
+
+    for r in range(len(s)):
+
+        charCount[s[r]] = 1 + charCount.get(s[r], 0)
 
          # This operation is at worst O(52) (since its a-z, A-Z)
         print('pre',l,r,minSize,minWindow,subCharCount,charCount)
-        while isSubset(subCharCount, charCount):
+        while l <= r and isSubset(subCharCount, charCount):
             if (r-l+1) < minSize:
                 minWindow = s[l:r+1]
                 minSize = len(minWindow)
 
+                # If the size of the window is the same as len(t), can early stop
+                if minSize == len(t):
+                    return minWindow
+
             #If the window is valid, then we'll decrease it by incrementing the left pointer
-            charCount[s[l]] =charCount.get(s[l],0)-1
+            charCount[s[l]] -=1
 
             l+=1
-
-        charCount[s[r]] = 1 + charCount.get(s[r], 0)
-        print('pos',l,r,minSize,minWindow,subCharCount,charCount)
-
-    #Check one last time
-    print(l,r,minSize,minWindow,subCharCount,charCount)
-    if isSubset(subCharCount, charCount) and len(s)-l < minSize:
-        minWindow = s[l:len(s)]
 
     return minWindow
 
@@ -71,4 +67,4 @@ if __name__ == "__main__":
     t = "AABC"
     out = "BANC"
 
-    print(minWindow('abba','a'))
+    print(minWindow('aa','aa'))
