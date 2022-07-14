47bb71630aa76306a2cd0bccc7eda55fa80972d4
Francisco Farinha <fquico1999@gmail.com>
2022-07-14 00:47:47 -0700

    Added linear time solution to maxSliding window with deque

--- maxSlidingWindow/maxSlidingWindow.py
+++ maxSlidingWindow/maxSlidingWindow.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-
+import collections
 """
 Difficulty: Hard
 
@@ -38,10 +38,39 @@ def maxSlidingWindow(nums, k):
     return maxWindow
 
 
+def maxSlidingWindowDeque(nums, k):
+    """
+    A deque - double ended queue - is a data structure that has fast appends/pops from either end - O(1)
+    With thus data structure, we can iterate over nums and ensure that only the largest element seen so far is in the deque
+    """
+    output = []
+    window = collections.deque() # Store indices so we can compare the left pointer directly to the left of the deque in O(1) time.
+
+    l = r = 0
+
+    while r < len(nums):
+        # Before we append nums[r], we need to ensure that every element in the deque
+        # Must be larger
+        while window and nums[window[-1]] < nums[r]:
+            window.pop() #O(1)
+
+        window.append(r) # O(1)
+        #At this point we can be certain that the first element of the deque is the largest
+
+        #Now we need to ensure that the left index is not out of bounds
+        if l > window[0]:
+            window.popleft()
+
+        #Ensure window is at least size k before adding to output
+        if (r+1)>= k:
+            output.append(nums[window[0]])
+            l+=1
+        r+=1
+    return output
 
 if __name__ == "__main__":
 
-    assert maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3) ==  [3,3,5,5,6,7]
-    assert maxSlidingWindow([1,1,1,1], 2) ==  [1,1,1]
-    assert maxSlidingWindow([5,4,3,2,1], 3) ==  [5,4,3], "Expected %s but instead got %s" % ([5,4,3], maxSlidingWindow([5,4,3,2,1], 3))
-    assert maxSlidingWindow([6,4,3,4,6], 1) == [6,4,3,4,6]
+    assert maxSlidingWindowDeque([1,3,-1,-3,5,3,6,7], 3) ==  [3,3,5,5,6,7]
+    assert maxSlidingWindowDeque([1,1,1,1], 2) ==  [1,1,1]
+    assert maxSlidingWindowDeque([5,4,3,2,1], 3) ==  [5,4,3], "Expected %s but instead got %s" % ([5,4,3], maxSlidingWindow([5,4,3,2,1], 3))
+    assert maxSlidingWindowDeque([6,4,3,4,6], 1) == [6,4,3,4,6]
