615dbb1ebf533c904c205daaaff866f959e7552c
Francisco Farinha <fquico1999@gmail.com>
2022-07-16 00:08:19 -0700

    Added constant time solution to minStack

--- minStack/minStack.py
+++ minStack/minStack.py
@@ -19,24 +19,44 @@ You must implement a solution with O(1) time complexity for each function.
 class MinStack:
 
     def __init__(self):
-        
+        self.stack = []     # One stack for actual values
+        self.minStack = []  # One stack for the minimum at the time of appending.
 
     def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.minStack:
+            self.minStack.append(val)
+        elif  val < self.minStack[-1]:
+            self.minStack.append(val)
+        else:
+            self.minStack.append(self.minStack[-1])
         
-
     def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
         
 
     def top(self) -> int:
-        
-
+        if self.stack:
+            return self.stack[-1]
+        else:
+            return None
     def getMin(self) -> int:
-        
+        if self.minStack:
+            return self.minStack[-1]
+        else:
+            return None
 
 
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(val)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
+if __name__ == "__main__":
+
+    # Your MinStack object will be instantiated and called as such:
+    obj = MinStack()
+    obj.push(3)
+    obj.push(1)
+    obj.push(5)
+    print(obj.top())
+    print(obj.getMin())
+    obj.pop()
+    param_3 = obj.top()
+    
