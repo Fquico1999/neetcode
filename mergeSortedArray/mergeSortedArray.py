aaba3ecdcff87f4c931add4c0b8e461cf598bfbe
Francisco <fquico1999@gmail.com>
2024-01-17 19:11:10 -0500

    Added refactored and slightly more efficient implementation

--- mergeSortedArray/mergeSortedArray.py
+++ mergeSortedArray/mergeSortedArray.py
@@ -7,7 +7,7 @@ The final sorted array should not be returned by the function, but instead be st
 """
 
 class Solution:
-    def merge(self, nums1, m, nums2, n) -> None:
+    def mergeOld(self, nums1, m, nums2, n) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
@@ -43,7 +43,21 @@ class Solution:
                 nums1[k] = nums2[j]
                 j-=1
             k-=1
-       
+    
+    def merge(self, nums1, m, nums2, n):
+        i = m-1
+        j = n-1
+        k = m+n-1
+
+        while(j>=0):
+
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i-=1
+            else:
+                nums1[k] = nums2[j]
+                j-=1
+            k-=1
 
 
 if __name__ == "__main__":
