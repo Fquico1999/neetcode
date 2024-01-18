842764a6bc60b60fba3f2e724123b4ad59686645
Francisco <fquico1999@gmail.com>
2024-01-18 02:41:58 -0500

    Added solution to removeElement

---
+++ removeElement/removeElement.py
@@ -0,0 +1,54 @@
+"""
+Difficulty: Easy
+
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. 
+The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+Consider the number of elements in nums which are not equal to val be k, to get accepted, 
+you need to do the following things:
+- Change the array nums such that the first k elements of nums contain the elements which are not equal to val. 
+- The remaining elements of nums are not important as well as the size of nums.
+- Return k.
+"""
+
+class Solution:
+    def removeElement(self, nums, val):
+
+        i = len(nums)-1 # pointer 1
+        j = i # pointer 2
+        k = 0 # Keep track of how many swaps
+        while (i >= 0):
+            if nums[i] == val:
+                k+=1
+                i-=1
+                j-=1
+            else:
+                while (nums[j] != val and j >= 0):
+                    j-=1
+                # Either got to the end, or found val
+                if j < 0:
+                    return len(nums) - k
+                elif nums[j] == val:
+                    #swap
+                    nums[j] = nums[i]
+                    nums[i] = val
+                    k+=1
+                    i-=1
+        # reached the end
+        return len(nums) - k
+
+
+if __name__ == "__main__":
+    test = Solution() 
+
+    nums = [0,1,2,2,3,0,4,2]
+    assert test.removeElement(nums, 2) == 5, "Expected 5"
+    assert nums[-3:] == [2,2,2], "Wrong"
+
+    nums = [3,2,2,3]
+    assert test.removeElement(nums, 2) == 2, "Expected 2"
+    assert nums[-2:] == [2,2], "Wrong"
+
+    nums = [0,1,2,2,3,0,4,2]
+    assert test.removeElement(nums, 2) == 5, "Expected 5"
+    assert nums[-3:] == [2,2,2], "Wrong"
