ca4b8551cf576afef5a5dd53b567f28abe77e6a9
Francisco <fquico1999@gmail.com>
2024-01-22 00:41:54 -0500

    setup for valid_anagram

---
+++ 242-validAnagram/test_valid_anagram.py
@@ -0,0 +1,24 @@
+"""
+Test suite for is_anagram
+"""
+
+from valid_anagram import Solution
+
+# Create solution object that can be used by all the tests
+sol = Solution()
+
+def test1():
+    """
+    First test from leetcode.
+    """
+    s ="anagram"
+    t = "nagaram"
+    assert sol.is_anagram(s, t) is True
+
+def test2():
+    """
+    Second test case from leetcode
+    """
+    s = "rat"
+    t = "car"
+    assert sol.is_anagram(s,t) is False
---
+++ 242-validAnagram/valid_anagram.py
@@ -0,0 +1,17 @@
+"""
+Difficulty: Easy
+
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+typically using all the original letters exactly once.
+"""
+
+class Solution(): #pylint: disable=too-few-public-methods
+    """
+    Solution class
+    """
+    def is_anagram(self, s, t)->bool:
+        """
+        Implementation of is_anagram.
+        """
+        
