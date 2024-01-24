376f9ade3ece5f7b4f1ed1a4485472e78591f847
Francisco <fquico1999@gmail.com>
2024-01-23 22:43:40 -0500

    linear time and space solution

--- 42-trappingRainWater/trapping_rain_water.py
+++ 42-trappingRainWater/trapping_rain_water.py
@@ -11,5 +11,28 @@ class Solution: #pylint: disable=too-few-public-methods
     """
     def trap(self, height)->int:
         """
-        Implementation
+        O(n) implementation using two extra arrays. These contain the max
+        height seen from left-to-right and right-to-left respectively.
+        O(n) per iteration over height, so O(3n)~O(n) and O(2n) in space.
         """
+        # If there is only one height, return zero
+        if len(height) < 2:
+            return 0
+
+        trap_sum = 0
+
+        # These will contain the max height seen from left to right
+        # and right to left.
+        max_l_arr = [height[0]]*len(height)
+        max_r_arr = [height[-1]]*len(height)
+
+        for i in range(1, len(height)):
+            max_l_arr[i] = max(max_l_arr[i-1], height[i])
+
+        for i in range(len(height)-2, -1, -1):
+            max_r_arr[i] = max(max_r_arr[i+1], height[i])
+
+        for i, h in enumerate(height):
+            trap_sum += max(min(max_l_arr[i], max_r_arr[i]) - h, 0)
+
+        return trap_sum
